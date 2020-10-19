5368ab6557b50bb708fb505a1d35c5621f4ba4c4
BajacDev <baptiste.jacquemot@orange.fr>
2020-10-19 11:40:19 +0200

    add new test

--- src/test/scala/mathgraph/LogicLayerTest.scala
+++ src/test/scala/mathgraph/LogicLayerTest.scala
@@ -13,4 +13,15 @@ class LogicLayerTest extends AnyFunSuite {
       val logicLayer4 = logicLayer3.addTruth(pos3, true)
       assert(logicLayer4.getAbsurd)
   }
+
+  test("'(false -> true) -> false' is absurd") {
+    // todo: make it less ugly
+      val logicLayer1 = new LogicLayer().init
+      val (pos2, logicLayer2) = logicLayer1.setApply(logicLayer1.implyPos, logicLayer1.falsePos)
+      val (pos3, logicLayer3) = logicLayer2.setApply(pos2, logicLayer2.truePos)
+      val (pos4, logicLayer4) = logicLayer3.setApply(logicLayer3.implyPos, pos3)
+      val (pos5, logicLayer5) = logicLayer4.setApply(pos4, logicLayer4.falsePos)
+      val logicLayer6 = logicLayer5.addTruth(pos5, true)
+      assert(logicLayer6.getAbsurd)
+  }
 }
